9493a5d8a6fffac4a643a5cb9219ef564864470b
Daniel Abreo <34114190+danielabreo@users.noreply.github.com>
2019-07-11 22:10:07 -0500

    Update verifydata.py

--- verifydata.py
+++ verifydata.py
@@ -5,6 +5,7 @@ coords = open("coordinates.csv",mode='r',encoding='utf-8-sig')
 coordslist = [row.split(',')[0] for row in coords]
 coords.seek(0)
 latlnglist = [row.split(',',1)[1].strip() for row in coords]
+coords.close()
 
 # creates list of all unique stop names listed in routes.csv
 routes = open("routes.csv",mode='r',encoding='utf-8-sig')
@@ -14,6 +15,7 @@ for i in fullrouteslist: # removes blanklines and route names
     if i.split(',')[1].isnumeric():
         routenames.append(i.split(',')[0])
 routeslist = list(dict.fromkeys(routenames)) # removes duplicates from list
+routes.close()
 
 print("Found only in routes.csv:",(set(routeslist).difference(coordslist)))
 print("Found only in coordinates.csv:",(set(coordslist).difference(routeslist)))
