78fcf9e991e9a97787e1656836d4e38fd5ff8bfd
Daniel Abreo <34114190+danielabreo@users.noreply.github.com>
2019-07-24 15:17:14 -0500

    Update generategraph.py

--- generategraph.py
+++ generategraph.py
@@ -7,6 +7,9 @@ stops=[row.split(',')[0] for row in open("coordinates.csv",mode='r',encoding='ut
 # creates list all walking connections
 walk=[row.rstrip().split(',') for row in open("walk.csv",mode='r',encoding='utf-8-sig')]
 
+# creates list of all stop coordinates
+print([[row.rstrip().split(',')[0],float(row.rstrip().split(',')[1]),float(row.rstrip().split(',')[2])] for row in open("coordinates.csv",mode='r',encoding='utf-8-sig')]
+
 graph={}
 for line in stops:
     currentbus=0
