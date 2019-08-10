67321e405b17969d4f5f525bfb1d168b4ed037ee
Daniel Abreo <34114190+danielabreo@users.noreply.github.com>
2019-08-09 22:07:04 -0500

    Update generategraph.py

--- generategraph.py
+++ generategraph.py
@@ -25,13 +25,39 @@ for line in stops:
 
 # adds walking connections between bus stops
 for line in walk:
-    if line[0] in graph[line[1]]:
-        graph[line[1]][line[0]].append(-1)
-    else:
+    if line[0] not in graph[line[1]]:
         graph[line[1]][line[0]] = [int(line[2]), -1]
-    if line[1] in graph[line[0]]:
-        graph[line[0]][line[1]].append(-1)
-    else:
+    if line[1] not in graph[line[0]]:
         graph[line[0]][line[1]] = [int(line[2]), -1]
 
+# adds connections within each bus route
+i = 0
+while i < (len(routes)-1):
+    if not routes[i][1] and routes[i+1][1]:
+        stopList = []
+        curBus = int(routes[i][0].split()[0])
+        while routes[i+1][1]:
+            stopList.append((routes[i+1][0],routes[i+1][1]))
+            i += 1
+        toAdd = {}
+        j = 0
+        while j < len(stopList):
+            k = (j+1)%len(stopList)
+            curTotal = int(stopList[j][1])-1
+            toAdd[stopList[j][0]] = {}
+            while stopList[j] != stopList[k]:
+                toAdd[stopList[j][0]][stopList[k][0]] = [curTotal,curBus]
+                if curTotal > 1:
+                    curTotal += int(stopList[k][1])-1 # favors staying on the same bus
+                else:
+                    curTotal += int(stopList[k][1])
+                k  = (k+1)%len(connections)
+            j += 1
+        for stop in toAdd:
+            for connection in toAdd[stop]:
+                if connection not in graph[stop]:
+                    graph[stop][connection] = toAdd[stop][connection]
+    else:
+        i += 1
+  
 print(graph)
