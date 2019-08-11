d4dd14cef97aca4248de998b43401864b02a4078
Daniel Abreo <34114190+danielabreo@users.noreply.github.com>
2019-08-11 09:31:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 api = Api(app)
 
 class Version(Resource):
-    def get(self):
-        return '1.q.1 - Aug 15, 2019'
+  def get(self):
+    return '1.0.0 - August 10, 2019'
 
 api.add_resource(Version, '/version')
 
 if __name__=='__main__':    
-    app.run(debug=True)
+  app.run(debug=True)
