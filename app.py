6232dbdbf4eec56657ee3da148cc4836ecaf704e
Daniel Abreo <34114190+danielabreo@users.noreply.github.com>
2019-08-11 15:06:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class Version(Resource):
