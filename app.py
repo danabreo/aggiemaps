f7110660456152fe8800cff76f49c985598f88a1
Daniel Abreo <34114190+danielabreo@users.noreply.github.com>
2019-08-11 16:31:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/version')
 def version():
-    return '2.2.3 - August 11, 2019'
+    return '2.4.0 - August 11, 2019'
 
 @app.route('/routes/<string:startLat>/<string:startLng>/<string:endLat>/<string:endLng>')
 def routes(startLat, startLng, endLat, endLng):
